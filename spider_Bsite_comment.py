e70e5b05db54bc36eb9ceda2241407358a7563c8
SummerTwilight <ncutzl@qq.com>
2019-08-23 17:59:29 +0800

    achieve data_save

--- spider_Bsite_comment.py
+++ spider_Bsite_comment.py
@@ -35,6 +35,8 @@ def get_comment_info(oid, pn, headers):
     '''
     start_url = 'https://api.bilibili.com/x/v2/reply?type=1&pn={}&oid=' + oid
     # 遍历爬取pn页评论
+    dict_comment = {}
+    k = 0
     for i in range(pn):
         if i != 0:
             try:
@@ -47,8 +49,7 @@ def get_comment_info(oid, pn, headers):
                 # TODO   爬取  评论点赞数  修改时间为时间轴
                 data_list = json.loads(data_json)
                 comment_dict = data_list["data"]["replies"]
-                dict_comment = {}
-                for j in range(20):
+                for j in range(0, 20):
                     dict_temp = {'mid': comment_dict[j]['mid'], 'uname': comment_dict[j]['member']['uname'],
                                  'sex': comment_dict[j]['member']['sex'], 'sign': comment_dict[j]['member']['sign'],
                                  'current_level': comment_dict[j]['member']['level_info']['current_level'],
@@ -56,26 +57,23 @@ def get_comment_info(oid, pn, headers):
                                  'vipDueDate': comment_dict[j]['member']['vip']['vipDueDate'],
                                  'ctime': comment_dict[j]['ctime'], 'rcount': comment_dict[j]['count'],
                                  'message': comment_dict[j]['content']['message']}
-                    dict_comment.update({j: dict_temp})
-
-                # 将整理后的将数据放入DataFrame中
-                user_comment_data = pd.DataFrame(dict_comment).T         # 转置一下
-                user_comment_data.index = range((i-1)*20+1, (i-1)*20+len(user_comment_data)+1)      # 为数据重新编号
-
-                # 将数据重新转为json后存储
-                comment_data_json = user_comment_data.to_json(orient='index')
-                # with open("./data/total_comment.json", "a", encoding="utf-8") as f:
-                #     f.write('\n')
-                #     f.write(comment_data_json)
+                    dict_comment.update({k: dict_temp})
+                    k = k + 1
             except:
                 traceback.print_exc()
                 continue
 
+    # 将整理后的将数据放入DataFrame中
+    user_comment_data = pd.DataFrame(dict_comment).T  # 转置一下
+    # user_comment_data.index = range((i - 1) * 20 + 1, (i - 1) * 20 + len(user_comment_data) + 1)  # 为数据重新编号
 
-    infot = open("./data/total_comment.json", encoding="utf-8").read()
-    comment_data_temp = json.loads(infot)
-    comment_data_df = pd.read_json(comment_data_temp)
-    return comment_data_df
+    # 将数据重新转为json后存储
+    comment_data_json = user_comment_data.to_json(orient='index')
+    with open('./data/total_comment.json', "w", encoding="utf-8") as file_data:
+        file_data.write('\n')
+        file_data.write(comment_data_json)
+
+    return user_comment_data
 
 
 def main():
@@ -93,11 +91,11 @@ def main():
     pn1_url = 'https://api.bilibili.com/x/v2/reply?type=1&pn=1&oid=' + oid
     pn1_html = get_html_text(pn1_url, headers)
     count = re.findall(r'20,"count":\d+', pn1_html)[0][11:]
-    pn = float(count)/20
+    pn = float(count) / 20
 
     # 得到视频评论的json信息，并返回。
-    comment_data_json = get_comment_info(oid, 5, headers)  # int(pn)
-    a = 1
+    user_comment_data = get_comment_info(oid, 5, headers)  # int(pn)
+    a = user_comment_data
 
 
 if __name__ == '__main__':
